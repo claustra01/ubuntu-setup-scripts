f92735bed6616cde7f16c09d016492dff1dab0e6
claustra01 <shinya430.g@gmail.com>
2024-08-14 23:01:29 +0900

    update ctf.sh

--- tools/ctf.sh
+++ tools/ctf.sh
@@ -1,5 +1,6 @@
-sudo apt -y install file strings checksec
+sudo apt -y install file strings checksec binwalk
 
+# gdb-peda
 sudo apt -y install gdb
 git clone https://github.com/longld/peda.git ~/peda
 echo "source ~/peda/peda.py" >> ~/.gdbinit
